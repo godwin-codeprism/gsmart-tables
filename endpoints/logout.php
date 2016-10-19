d2e10cfc4b094c6d3d150fafd5a0b81fcb0c8d9c
godwinvc <godwin@swift.local>
2016-10-19 12:01:06 +0530

    gitignore updated

--- endpoints/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include('connection.php');
-    $data = json_decode(file_get_contents("php://input"));
-    $token = $data -> token;
-    $cleanToken = str_replace('"','',$token);
-    $db -> query("UPDATE qlsusers SET token='LOGGED_OUT' WHERE token='$cleanToken'");
-?>
